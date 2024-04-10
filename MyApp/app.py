474870006e6a7c78800759dc0565a41812bfb6aa
Trương Văn Thông <thongvanpro2002@gmail.com>
2024-04-10 13:51:49 +0700

    update

---
+++ MyApp/app.py
@@ -0,0 +1,63 @@
+
+import random
+import flask
+from flask import request, make_response, jsonify
+from qdrant_client import QdrantClient
+
+app = flask.Flask(__name__)
+
+
+def search(query):
+    # Đặt tên collection bạn đã tạo khi thực hiện pipeline vào biến bên dưới
+    collection_name = '20001955'
+    # thực hiện kết nối với cơ sở dữ liệu qdrant
+    client = QdrantClient(host='qdrant_db', port=6333)
+    collections = client.get_collections()
+    collection_names = [collection.name for collection in collections.collections]
+    embedding = [random.random() for i in range(1536)]
+    
+    if collection_name in collection_names:
+        # thực hiện tìm kiếm với vector embedding bên trên
+        results = client.search(
+            collection_name=collection_name, query_vector=embedding, limit=1
+        )
+        if results:
+            result_json = results[0].model_dump()
+            return {
+                "status": "success",
+                "query": query,
+                "result": result_json
+            }
+        else:
+            return {
+                "status": "error",
+                "message": "No results found"
+            }
+    return {"message": "Collection not found"}
+
+
+@app.route('/health', methods=['GET'])
+def healthCheck():
+    # điền mssv của bạn vào bên dưới
+    return jsonify({
+        "status": "success",
+        "student_id": 20001955 
+    })
+
+# Viết api có route "/search", phương thức POST để nhận vào câu query từ phía ngưới dùng
+# sử dụng hàm search bên trên để tìm kiếm và trả về kết quả của hàm search cho người dùng
+@app.route('/search', methods=['POST'])
+def searchView():
+    query = request.json['query']
+    results = search(query)
+    return make_response(results)
+
+@app.route('/', methods=['GET'])
+def home():
+    return {"message": "ok"}
+
+if __name__ == '__main__':
+    # run app với port là "99xy" với xy là 2 số cuối cùng của mssv của bạn. Ví dụ: mssv: 17101691 thì port=9991
+    app.run(host='0.0.0.0', port=9955)
+
+# curl -X POST http://localhost:8989/search -H "Content-Type: application/json" -d '{"query": "hội nghị cấp khoa"}'
---
+++ pipeline.py
@@ -0,0 +1,215 @@
+import uuid
+import pymongo
+import datetime as dt
+import json
+import random
+
+# from midtermDAG import DAG
+# from midtermOperator import BashOperator
+# from midtermOperator import PythonOperator
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams, PointStruct
+
+# Hoàn thành việc kết nối với MongoDB và Qdrant
+qdrant_client = QdrantClient(host="qdrant_db", port=6333)
+mongo_client = pymongo.MongoClient("mongodb://admin:admin@mongodb:27017")
+database_mongo = mongo_client["midterm"]
+
+# Tạo collection có tên là mssv của bạn trong MongoDB và Qdrant
+# Ví dụ mssv của bạn là 17101691 thi tên collection sẽ là "17101691"
+collection_mongo = database_mongo["20001955"]
+name_collection_qdrant = '20001955'
+
+def create_collection_qdrant():
+    try:
+        # lấy tên tất cả collection hiện có trong Qdrant
+        collections = qdrant_client.get_collections()
+        collectionNames = [
+            collection.name for collection in collections.collections
+        ]
+
+        # cấu hình vector params cho collection bao gồm size = 1536 và distance = cosine
+        vectorParams = {
+            'size': 1536,
+            'distance': Distance.COSINE
+        }
+        ### YOUR CODE HERE ###
+        # kiểm tra nếu collection chưa tồn tại thì tạo mới
+        # Sử dụng **vectorParams để unpack dict thành các keyword arguments
+        if name_collection_qdrant not in collectionNames:
+            qdrant_client.recreate_collection(
+                collection_name=name_collection_qdrant,
+                vectors_config=VectorParams(**vectorParams)
+                )
+            return {
+                "status": "success",
+                "collection": name_collection_qdrant,
+                "vectorParams": str(vectorParams)
+            }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
+
+def insert_data_mongoDB():
+    try:
+        # đọc dữ liệu từ file data_iuh_new.json và chọn ngẫu nhiên điểm dữ liệu gán vào biến data
+        message = ""
+        with open("/opt/airflow/dags/data_iuh_new.json", "r") as f:
+            data = random.choice(json.load(f))
+        # kiểm tra title của điểm dữ liệu đã tồn tại trong MongoDB chưa
+        check_exist = bool(collection_mongo.find_one({'title': data['title']}))
+        # nếu đã tồn tại thì gán "Data already exists" cho biến message
+        if check_exist:
+            message = "Data already exists"
+        # nếu chưa thì thêm trường "status": "new" vào data và insert vào MongoDB
+        # sau đó gán "Data inserted" cho biến message
+        if not check_exist:
+            data['status'] = 'new'
+            collection_mongo.insert_one(data)
+            message = 'Data inserted'
+
+        title = data["title"]
+        return {
+            "status": "success",
+            "data": title,
+            "message": message
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
+
+
+def insert_data_qdrant():
+    try:
+        # lấy tất cả các điểm dữ liệu có "status":"new" từ MongoDB
+        allNewData = collection_mongo.find({'status': 'new'})
+        # insert các điểm dữ liệu này vào Qdrant (lưu ý: không insert trường "_id" và "embedding" của MongoDB vào Qdrant)
+        # sau khi insert thành công thì cập nhật trường "status":"indexed" cho các điểm dữ liệu đã insert trong MongoDB
+        for new in allNewData:
+            # Loại bỏ các trường "_id" và "embedding" của MongoDB
+            new.pop('_id')
+            vector = new.pop('embedding')
+            point = PointStruct(id=str(uuid.uuid4()),
+                    vector=vector,
+                    payload=new)
+            # Chèn dữ liệu vào Qdrant
+            qdrant_client.upsert(collection_name='20001955', points=[point])
+            # Cập nhật trường "status":"indexed" cho các điểm dữ liệu đã insert trong MongoDB  
+            collection_mongo.update_many({"status": "new"}, {"$set": {"status": "indexed"}})
+                   
+        return {
+            "status": "success",
+            "message": "Data inserted to Vector DB successfully"
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
+
+
+def count_data():
+    try:
+        count = collection_mongo.count_documents({})
+        count_indexed = collection_mongo.count_documents({"status": "indexed"})
+        count_new = collection_mongo.count_documents({"status": "new"})
+        return {
+            "status": "success",
+            "indexed": count_indexed,
+            "new": count_new,
+            "mongoDB": count,
+            "vectorDB": qdrant_client.get_collection(collection_name=name_collection_qdrant).points_count
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
+
+
+def search_by_vector():
+    try:
+        # tạo ngẫu nhiên một vector có size = 1536 và sử dụng Qdrant để tìm kiếm 1 điểm gần nhất
+        result = [random.random() for i in range(1536)]
+
+        result_json = result[0].model_dump()
+        return {
+            "status": "success",
+            "result": result_json 
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
+
+# đặt owner là mssv của bạn, Thử lại 1 lần nếu thất bại, thời gian chờ giữa các lần thử là 1 phút
+default_args = {
+    'owner': '20001955',
+    'start_date': dt.datetime.now() - dt.timedelta(minutes=2),
+    'retries': 1,
+    'retry_delay': dt.timedelta(minutes=1),
+    'student_id': 20001955
+}
+
+# Khởi tạo DAG với tên là mssv của bạn và cài đặt mỗi 5 phút chạy 1 lần
+
+with DAG('20001955',
+         default_args=default_args,
+         tags=['midterm'],
+         schedule_interval=dt.timedelta(minutes=5), 
+         ) as dag:
+
+    # khởi tạo pipeline sử dụng BashOperator và PythonOperator như sau:
+    # task 1: sử dụng BashOperator để in ra "Midterm exam started" với task_id là mssv của bạn (ví dụ: task_id='17101691')
+    task1 = BashOperator(
+        task_id='20001955',
+        bash_command='echo "Midterm exam started"',
+    )
+    
+    # task 2: sử dụng PythonOperator để tạo collection trong Qdrant với task_id là 4 chữ số đầu của mssv của bạn (ví dụ: task_id='1710')
+    task2 = PythonOperator(
+        task_id='2000',
+        python_callable=create_collection_qdrant,
+    )
+   
+    # task 3: sử dụng PythonOperator để insert data vào MongoDB với task_id là 3 chữ số cuối của task2 và số kế tiếp trong mssv (ví dụ: task_id='7101')
+    task3 = PythonOperator(
+        task_id='0019',
+        python_callable=insert_data_mongoDB,
+    )
+ 
+    # task 4: sử dụng PythonOperator để insert data vào Qdrant với task_id là 3 chữ số cuối của task3 và số kế tiếp trong mssv (ví dụ: task_id='1016')
+    task4 = PythonOperator(
+        task_id='0195',
+        python_callable=insert_data_qdrant,
+    )
+    
+    # task 5: sử dụng PythonOperator để thực hiện hàm count_data với task_id là 3 chữ số cuối của task4 và số kế tiếp trong mssv (ví dụ: task_id='0169')
+    task5 = PythonOperator(
+        task_id='1955',
+        python_callable=count_data,
+    )
+    
+    # task 6: sử dụng PythonOperator để thực hiện search bằng vector với task_id là 3 chữ số cuối của task5 và số kế tiếp trong mssv (ví dụ: task_id='1691')
+    task6 = PythonOperator(
+        task_id='9550',
+        python_callable=search_by_vector,
+    )
+    
+    # task 7: sử dụng BashOperator để in ra "Midterm exam ended" với task_id là 2 số đầu và 2 số cuối của mssv (ví dụ: task_id='1791')
+    task7 = BashOperator(
+        task_id='2095',
+        bash_command='echo "Midterm exam ended"',
+    )
+
+task1 >> task2 >> task3 >> task4 >> task5 >> task6 >> task7
